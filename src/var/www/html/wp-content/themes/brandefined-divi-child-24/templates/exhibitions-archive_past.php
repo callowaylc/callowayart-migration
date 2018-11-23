e11b58c7b68aa7ce2a2b3ab499ebdd3dbb2f47a0
christian calloway <christian.calloway@sprinklr.com>
2018-11-23 11:20:46 -0800

    Fix Current Exhibit View

--- src/var/www/html/wp-content/themes/brandefined-divi-child-24/templates/exhibitions-archive_past.php
+++ src/var/www/html/wp-content/themes/brandefined-divi-child-24/templates/exhibitions-archive_past.php
@@ -9,8 +9,7 @@ $args = array (
  	'tax_query' => [[
     'taxonomy' => 'exhibition-category',
     'field'    => 'slug',
-    'terms'    => ['upcoming-exhibitions', 'current-exhibitions'],
-    'operator' => 'NOT IN',
+    'terms'    => ['past-exhibitions'],
 	]]
 );
 
@@ -22,32 +21,16 @@ $query = new WP_Query( $args ); ?>
 				<!--<div class="past-exhibitions bd-row">-->
 <?php
 
-if ( $query->have_posts() ):
-	while ( $query->have_posts() ):
-		$query->the_post();
-
-		// check for current exhibitions
-		$cat_tax = 'exhibition-category';
-		$is_past = has_term('past-exhibitions', $cat_tax);
-
-		// diplay past exhibitions if available
-		if(true) : ?>
-
-		<?php  if(true) : ?>
-			<?php get_template_part( 'templates/exhibitions', 'archive_past_view' ); ?>
-		<?php $exhibitions = $exhibitions+1; endif?>
-
- <?php endif;
-
-	endwhile; // post query
-else:
-
-endif;
+while ( $query->have_posts() ) {
+	$query->the_post();
+	get_template_part( 'templates/exhibitions', 'archive_past_view' );
+}
 
 // Restore original Post Data
 wp_reset_postdata();
 wp_reset_query();
 ?>
+
 <!--</div>-->
 			</div>
 		</div>
